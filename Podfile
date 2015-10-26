08cd811030a7ffdf8fbe25bb2fd3d5f0b105cd92
Steve Caine <scaine@mac.com>
2015-10-26 17:29:31 -0400

    added AFNetworking & Mantle via CocoaPods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '7.0'
+
+target 'MBTA-Mantle' do
+	pod 'AFNetworking',	'~> 2'
+	pod 'Mantle',		'~> 2'
+end
+
+target 'MBTA-MantleTests' do
+end
+
---
+++ Pods/Target Support Files/Pods-MBTA-Mantle-Mantle/Pods-MBTA-Mantle-Mantle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MBTA_Mantle_Mantle : NSObject
+@end
+@implementation PodsDummy_Pods_MBTA_Mantle_Mantle
+@end
